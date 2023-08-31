58a25a7383fccdb5d3e10b7a58417ab5cd6ba890
suptohossein <supto.hossein@gmail.com>
2023-08-31 16:17:57 +0600

    working on blog page

--- src/App.vue
+++ src/App.vue
@@ -13,20 +13,10 @@ import { RouterLink, RouterView } from 'vue-router'
             aria-controls="mobile-menu" aria-expanded="false">
             <span class="absolute -inset-0.5"></span>
             <span class="sr-only">Open main menu</span>
-            <!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
             <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
               aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
-            <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
             <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
               aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -37,11 +27,6 @@ import { RouterLink, RouterView } from 'vue-router'
           <div class="flex flex-shrink-0 items-center">
             <h3 class="font-bold">MR BLOG</h3>
           </div>
-
-          <!-- menu -->
-
-          <!-- menu -->
-
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
@@ -112,19 +97,57 @@ import { RouterLink, RouterView } from 'vue-router'
           to="/">বিজ্ঞান</RouterLink>
         <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
           to="/">বিজ্ঞানী</RouterLink>
-      <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-        to="/">উপকথা</RouterLink>
+        <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/">উপকথা</RouterLink>
 
+      </div>
+    </div>
+  </nav>
+
+
+  <div class="mx-auto max-w-7xl mt-10 mb-10">
+    <div class="border rounded-t">
+      <div class="relative">
+        <img
+          src="https://images.unsplash.com/photo-1692902295604-6c5109aa55a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80"
+          alt="">
+      </div>
+      <div class="">
+        <h2 class="text-4xl font-bold p-5 text-white text-center absulate bottom-10 left-10 bg-green-900 mb-2 w-1/3">তামিমের বিকল্প ভাবার</h2>
+        <h3 class="text-4xl font-bold p-5 text-white text-center absulate bottom-10 left-10 bg-green-900 mb-2 w-1/3">সময়তো নেই!</h3>
+      </div>
+    </div>
+
+    <div class="p-10">
+      <div class="">
+        <h2 class="font-bold text-xl mb-10">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, sit.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quos illum sequi reiciendis ipsa pariatur,
+          laudantium officiis? Qui, sunt consequuntur iste magni, saepe accusamus cumque tempora labore quos nihil
+          provident expedita vel atque corrupti quae voluptatibus eius, explicabo velit aspernatur dicta sint inventore
+          deserunt voluptatum quasi? Quas nostrum autem est atque necessitatibus, praesentium enim totam iure,
+          exercitationem esse inventore. Impedit mollitia labore, aliquid explicabo pariatur id eligendi ratione sed
+          deserunt libero aperiam quam perspiciatis culpa quia vitae saepe vel qui a non! Dolore saepe, assumenda pariatur
+          odit cumque provident! Nostrum eum maxime alias, nulla vel quos molestias ipsa blanditiis rerum, libero ipsum
+          nesciunt delectus! Molestiae ipsum id veniam ipsa nemo excepturi ducimus dicta quod quibusdam explicabo autem
+          quo veritatis laborum aspernatur necessitatibus dolorem dolorum, molestias tenetur. Deleniti nostrum suscipit
+          maxime accusantium impedit atque autem aperiam aliquam? Obcaecati veritatis facere sed, enim eius soluta dolore
+          totam corporis voluptates similique rerum blanditiis doloribus quidem id ex repellat assumenda, sunt accusamus
+          quae unde vel sit a illo nihil. Iusto, dignissimos veniam id consequatur aliquam iure pariatur accusantium
+        voluptatem eligendi nostrum, error dolore eaque possimus voluptate rerum numquam expedita similique mollitia
+        totam quo temporibus distinctio, eum placeat. Laborum itaque dolorem animi pariatur nihil est magni explicabo
+        porro rerum dolorum assumenda fugit officiis, tempora voluptas earum quia? Soluta sint, commodi exercitationem
+        quam officiis beatae natus magnam magni blanditiis officia quas laborum voluptatum cumque alias velit
+        repudiandae architecto delectus eos animi eligendi adipisci. Architecto rerum, labore dolores temporibus laborum
+        pariatur quam excepturi, sed ut corporis officia praesentium! Quo, eaque cupiditate? Commodi aut id illum
+        recusandae tempora deleniti in dolorum consequatur sapiente? Quos nulla illo aut mollitia ullam, nostrum sed ea
+        fugiat dolores velit, assumenda incidunt, maiores eum obcaecati provident! Provident impedit eligendi officiis
+        nam nobis praesentium vel accusamus. Quibusdam praesentium culpa voluptatem ut sequi laboriosam et!</p>
     </div>
   </div>
-</nav>
+</div>
 
 
 
-
-
-<RouterView />
-
-</template>
+<RouterView /></template>
 
 
