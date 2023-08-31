4b570fd4cd515760bc5c76ad6ddc65fe85d73833
suptohossein <supto.hossein@gmail.com>
2023-08-31 15:49:15 +0600

    integrate tailwindcss and design navbar

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,130 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <nav class="bg-white">
+    <div class="mx-auto max-full px-2 sm:px-6 lg:px-8 border-t-2 border-b-2 border-gray-300">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <button type="button"
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            aria-controls="mobile-menu" aria-expanded="false">
+            <span class="absolute -inset-0.5"></span>
+            <span class="sr-only">Open main menu</span>
+            <!--
+            Icon when menu is closed.
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+            Menu open: "hidden", Menu closed: "block"
+          -->
+            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+              aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            <!--
+            Icon when menu is open.
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+            Menu open: "block", Menu closed: "hidden"
+          -->
+            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+              aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex flex-shrink-0 items-center">
+            <h3 class="font-bold">MR BLOG</h3>
+          </div>
+
+          <!-- menu -->
+
+          <!-- menu -->
+
+        </div>
+        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+
+          <!-- Profile dropdown -->
+          <div class="relative ml-3">
+
+            <div class="hidden sm:ml-6 sm:block">
+              <div class="flex space-x-4">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <RouterLink class="bg-gray-300 text-gray-900 rounded-md px-3 py-2 text-sm font-medium" to="/">হোম
+                </RouterLink>
+                <RouterLink
+                  class="text-gray-900 hover:bg-gray-300 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
+                  to="/">ইতিহাস</RouterLink>
+                <RouterLink
+                  class="text-gray-900 hover:bg-gray-300 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
+                  to="/">লাইব্রেরী</RouterLink>
+                <RouterLink
+                  class="text-gray-900 hover:bg-gray-300 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
+                  to="/">খেলাধুলা</RouterLink>
+                <RouterLink
+                  class="text-gray-900 hover:bg-gray-300 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
+                  to="/">বাংলাদেশ</RouterLink>
+                <RouterLink
+                  class="text-gray-900 hover:bg-gray-300 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
+                  to="/">বিশ্ব</RouterLink>
+                <RouterLink
+                  class="text-gray-900 hover:bg-gray-300 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
+                  to="/">বই ও ছিনেমা</RouterLink>
+                <RouterLink
+                  class="text-gray-900 hover:bg-gray-300 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
+                  to="/">বিজ্ঞান</RouterLink>
+                <RouterLink
+                  class="text-gray-900 hover:bg-gray-300 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
+                  to="/">বিজ্ঞানী</RouterLink>
+                <RouterLink
+                  class="text-gray-900 hover:bg-gray-300 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium"
+                  to="/">উপকথা</RouterLink>
+
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
 
-  <RouterView />
+    <!-- Mobile menu, show/hide based on menu state. -->
+    <div class="sm:hidden" id="mobile-menu">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+        <RouterLink class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" to="/">হোম</RouterLink>
+        <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/">ইতিহাস</RouterLink>
+        <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/">লাইব্রেরী</RouterLink>
+        <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/">খেলাধুলা</RouterLink>
+        <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/">বাংলাদেশ</RouterLink>
+        <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/">বিশ্ব</RouterLink>
+        <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/">বই ও ছিনেমা</RouterLink>
+        <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/">বিজ্ঞান</RouterLink>
+        <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/">বিজ্ঞানী</RouterLink>
+      <RouterLink class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+        to="/">উপকথা</RouterLink>
+
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+<RouterView />
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
